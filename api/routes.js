e7e62118be1baa0388610d16d558672a1fb21ed3
Esther Urzanqui <estherurzanqui@Esthers-MBP.home>
2020-10-14 18:24:39 +0200

    ci: add JWT

--- api/routes.js
+++ api/routes.js
@@ -1,13 +1,27 @@
 const express = require('express')
+const routes = express.Router()
+const jwt = require("jsonwebtoken")
 
 const db = require('./lib/helper')
 
-const routes = express.Router()
+
 
 routes.get('/', (req, res) => {
     res.send({
         message: 'test'
     })
 })
+
+//log in
+routes.post("/login", function (req, res, next) {
+    const [user_name, password] = req.body;
+})
+
+//send private info to the user
+routes.get("/profile", function (req, res, next) {
+    
+})
+
+
 
 module.exports = routes
