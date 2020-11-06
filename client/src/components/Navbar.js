072e82d4fc6099feddc9a3a78da8865052e7fd63
Esther Urzanqui <estherurzanqui81@gmail.com>
2020-11-06 17:20:18 +0100

    ci: Navbar login and logout

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,23 +1,43 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import "../css/Navbar.css"
-import { Navbar, Nav, NavLink } from "reactstrap";
+import { Navbar, Nav, NavLink, NavbarBrand } from "reactstrap";
 
 
 const TheNavbar = (props) => {
+    console.log(props)
     return (
         <div className="navbar">
             <Navbar>
+                <NavbarBrand>
+                    <NavLink id="logo" tag={Link} exact to="/">
+                        <img src="/img/logounax.png" alt="image" style={{ width: 150 }} />
+                    </NavLink> 
+                </NavbarBrand>
                 <Nav>
-                    <NavLink tag={Link} exact to="/">
-                        <img src="/img/logounax.png" alt="image" style={{ width: 200 }} />
-                    </NavLink> 
-                    <NavLink tag={Link} exact to="/">
+                  {props.isLoggedIn ? (
+                    <Nav>
+                        <NavLink className="link-pink" tag={Link} exact to="/feed">
+                        Profile
+                        </NavLink>
+                        <NavLink
+                        className="link-pink"
+                        tag={Link}
+                        exact to="/"
+                        onClick={props.logout()}>
+                        Logout
+                        </NavLink>
+                    </Nav>
+                  ) : (
+                    <Nav>
+                        <NavLink className="link-pink" tag={Link} exact to="/">
                         Login
-                    </NavLink> 
-                    <NavLink tag={Link} exact to="/signin">
+                        </NavLink> 
+                        <NavLink className="link-pink" tag={Link} exact to="/signin">
                         Signin
-                    </NavLink>
+                        </NavLink>
+                    </Nav>
+                  )}   
                 </Nav>
             </Navbar>
         </div>
