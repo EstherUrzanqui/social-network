14868317f7d4c42264defe6994ef54d6c23a374b
Esther Urzanqui <estherurzanqui81@gmail.com>
2020-12-01 12:01:45 +0100

    ci: adding following component

---
+++ client/src/components/Following.js
@@ -0,0 +1,50 @@
+import React from "react";
+import axios from "axios";
+import withUser from "./Withuser"
+import { Button, Card, CardBody, CardTitle } from 'reactstrap'
+
+class Following extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      following: []
+    }
+  }
+
+  getFollowing = async () => {
+    const userId = this.props.user[0].id
+
+    try {
+      const response = await axios(`http://localhost:7001/api/users/${userId}/following`)
+      
+      this.setState({ 
+        following: response.data,
+      })
+      console.log(response.data)
+    } catch (error) {
+      this.setState({ error: true})
+    }
+  }
+
+  render() {
+    const { following } = this.state
+
+    return (
+      <div>
+          <h2>Following</h2>
+            <ul>
+              {following.map((follow, index) => {
+                return (
+                  <Card key= {index}>
+                    <CardBody>
+                      <CardTitle>{follow.user_name}</CardTitle>
+                    </CardBody>
+                  </Card>)
+              })}
+            </ul>
+        </div>
+    )
+  }
+}
+
+export default withUser(Following);
