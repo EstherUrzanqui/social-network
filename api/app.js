102276f7302073b0f8c36d9622b41213cc07860a
Esther Urzanqui <estherurzanqui81@gmail.com>
2020-12-08 12:18:26 +0100

    ci: add middleword

--- api/app.js
+++ api/app.js
@@ -2,22 +2,26 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const path = require('path')
+
+const clientPath = path.join(__dirname, '../', 'client', 'build')
 
 const apiRoutes = require('./routes')
 
 const app = express()
+app.use(express.static(clientPath))
+
 
 app.use(cors())
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/api', apiRoutes)
 
-app.get('/', (req, res) => {
-    res.send({
-        message: 'social works'
-    })
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(clientPath, 'index.html'))
 })
+
+app.use(express.static('./client/public/img'))
 
 app.listen(process.env.API_PORT, () => {
     console.log(`Starting server in PORT ${process.env.API_PORT}`)
