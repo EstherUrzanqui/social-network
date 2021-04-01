eab3c47b2a90e511d9a8c2887b9ff13d5ef2f34d
EstherUrzanqui <estherurzanqui81@gmail.com>
2021-04-01 20:50:17 +0200

    fix followers component, clear unused code

--- client/src/components/Followers.js
+++ client/src/components/Followers.js
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
 import withUser from "./Withuser"
-import { Button, Card, CardBody, CardTitle, CardImg } from 'reactstrap'
+import { Card, CardBody, CardTitle, CardImg } from 'reactstrap'
 import "../css/Followers.css"
 
 
@@ -9,130 +9,46 @@ class Followers extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      users: [],
-      //follower: [],
-      following: [], 
-      followingUsers: []
+      followers: [],
     }
   }
 
   componentDidMount = () => {
-    this.getUsers()
-    this.getFollowing()
-    
+    this.getFollowers()
   }
 
-  getUsers = async () => {
+  getFollowers = async () => {
     const userId = this.props.user[0].id
 
     try {
-      const response = await axios(`http://localhost:7001/api/users/${userId}/suggestions`)
-      this.setState({ users: response.data})
+      const response = await axios(`http://localhost:7001/api/users/${userId}/followers`)
+      this.setState({ followers: response.data})
+      console.log(response.data)
     } catch {
       this.setState({ error: true })
     }
   }
 
-  getFollowing = async () => {
-    const userId = this.props.user[0].id
-
-    try {
-      const response = await axios(`http://localhost:7001/api/users/${userId}/followers`)
-
-      const tempFollowing = response.data.map((follow, index) => {
-        return follow.user_name
-      })
-      this.setState({ 
-        following: response.data,
-        followingUsers: tempFollowing
-      })
-    } catch (error) {
-      this.setState({ error: true})
-    }
-  }
-
-  followUser = async (e) => {
-    //e.preventDefault();
-    const userId = this.props.user[0].id
-    const followedId = e.target.value
+  handleClick = (id) => {
+    this.props.history.push(`/allprofiles/${id}`)
     
-    try {
-      const response = await axios.post(`http://localhost:7001/api/users/${userId}/follow/${followedId}`, {
-        userId,
-        followedId,
-        createdAt: new Date().toISOString().slice(0, 10),
-        updatedAt: new Date().toISOString().slice(0, 10)
-      })
-      
-      console.log(response.data)
-      this.setState(state => ({
-        loggedIn: !state.loggedIn,
-      })) 
-    } catch(error) {
-      console.log(error)
-    }
   }
-
-  unfollowUser = async (e) => {
-    //e.preventDefault();
-    const userId = this.props.user[0].id;
-    const followedId = e.target.value;
- 
-    try {
-      const response = await axios.delete(
-        `http://localhost:7001/api/users/${userId}/unfollow/${followedId}`
-      );
-      console.log(response);
-      let array = [...this.state.following];
-      let index = array.indexOf(followedId);
-      if (index !== -1) {
-        array.splice(index, 1);
- 
-        const tempFollowing = array.map((follow, index) => {
-          return follow.followedId;
-        });
- 
-        this.setState({
-          following: array,
-          followingUsers: tempFollowing,
-        });
- 
-      }
-    } catch (error) {
-      this.setState({ error: true });
-    }
-  };
-
-  onFollow = (e) => {    
-    this.getFollowing();
-    this.followUser(e);
-  }
-
-  onUnfollow = (e) => {
-    this.getFollowing()
-    this.unfollowUser(e)
-  }
-
 
   render() {
-    const { users } = this.state
-    const userId = this.props.user[0].id
+    const { followers } = this.state
     
     return (
       <div>
-        <h2>Suggestions</h2>
         <ul>
-          {users.map((user, index) => {
-           if(user.id !== userId) {
+          {followers.map((follower, index) => {
              return (
               <Card className="users" key= {index}>
                 <CardBody>
-                  <CardImg className="picfollowers" top width="9%" src={user.image} />
-                  <CardTitle>{user.user_name}</CardTitle>
-                    <Button id="btn" value={user.id} onClick={this.onFollow}>follow</Button>
+                  <CardImg className="picfollowers" top width="9%" src={follower.image} />
+                  <CardTitle onClick={() => this.handleClick(follower.id)}>{follower.user_name}</CardTitle>
                 </CardBody>
               </Card>
-             )}  
+             )  
           })}
         </ul>
       </div>
